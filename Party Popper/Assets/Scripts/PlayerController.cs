b4a7e777a2062b37f521abff272fb7e57f40214e
Rick van Sloten <rickvansloten@gmail.com>
2018-09-19 10:25:10 +0200

    Developers Have Joined

---
+++ Party Popper/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Update ()
+    {
+		
+	}
+}
