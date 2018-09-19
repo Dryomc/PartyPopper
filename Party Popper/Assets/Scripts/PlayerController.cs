3d9e9fa00d229ff24d9e8ae3840892f01a24e4f5
Youri van den Broek <521251@student.glu.nl>
2018-09-19 10:53:54 +0200

    PlayerController
    Added basic movement for teh boi

--- Party Popper/Assets/Scripts/PlayerController.cs
+++ Party Popper/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
 
-	void Start ()
+namespace PartyPopper
+{
+    public class PlayerController : MonoBehaviour
     {
-		
-	}
+        private float _Horizontal;
+        private float _Vertical;
 
-    void FixedUpdate()
-    {
-        
+        void Start()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            transform.Translate(new Vector3(_Horizontal, 0, _Vertical) * 20 * Time.fixedDeltaTime);
+        }
+
+        void Update()
+        {
+            _Horizontal = Input.GetAxisRaw("Horizontal");
+            _Vertical = Input.GetAxisRaw("Vertical");
+        }
     }
 
-    void Update ()
-    {
-		
-	}
 }
