f9e53bdf310557fc86b0d0e16efa3a597489068c
Rick van Sloten <rickvansloten@gmail.com>
2018-09-19 12:25:17 +0200

    Revert "Revert "Ball Coloring Script""
    This reverts commit d0b026cfb33808a177eddfa0fcf2abbd97870b9a.

---
+++ Party Popper/Assets/Scripts/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartyPopper
+{
+    public enum Team
+    {
+        BLUE,
+        RED,
+        YELLOW,
+        GREEN,
+        NONE
+    }
+}
