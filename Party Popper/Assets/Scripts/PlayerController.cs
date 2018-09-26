828e0a42f624c2e445a65ed451a80e288fb34986
Youri van den Broek <521251@student.glu.nl>
2018-09-26 09:12:51 +0200

    Updated Player Controller
    Added controller support

--- Party Popper/Assets/Scripts/PlayerController.cs
+++ Party Popper/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,24 @@ namespace PartyPopper
         private float _Horizontal;
         private float _Vertical;
 
+        private float _HorizontalRotation;
+        private float _VerticalRotation;
+
 
         void FixedUpdate()
         {
-            transform.Translate(new Vector3(0, 0, _Vertical) * 20 * Time.fixedDeltaTime);
-            transform.Rotate(new Vector3(0, _Horizontal, 0) * 100 * Time.fixedDeltaTime);
+            transform.Translate(new Vector3(_Horizontal, 0, _Vertical) * 20 * Time.fixedDeltaTime);
+            //transform.Translate(new Vector3(, 0, 0) * 100 * Time.fixedDeltaTime);
+
+            transform.Rotate(new Vector3(0, _HorizontalRotation, 0) * 200 * Time.fixedDeltaTime);
         }
 
         void Update()
         {
             _Horizontal = Input.GetAxis("Horizontal");
             _Vertical = Input.GetAxis("Vertical");
+
+            _HorizontalRotation = Input.GetAxis("HorizontalRotation");
         }
 
         public Vector3 GetNormal()
